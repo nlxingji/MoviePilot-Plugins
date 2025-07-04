08f7fed213f50329f11a4a0110b156759f3ceb17
jimmy <nlxingji@gmail.com>
2025-07-04 22:41:12 +0800

    init

--- plugins.v2/pathmonitor/__init__.py
+++ plugins.v2/pathmonitor/__init__.py
@@ -30,7 +30,7 @@ class PathMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "Linkease_A.png"
     # 插件版本
-    plugin_version = "3.0.0"
+    plugin_version = "3.0.1"
     # 插件作者
     plugin_author = "nlxingji"
     # 作者主页
